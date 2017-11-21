60ff91013f1560cc240c76eef0f62fbac8234369
leslie1943 <leslie43@sina.com>
2017-11-21 23:11:43 +0800

    reg
    reg

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -16,18 +16,15 @@ import { HomePage } from '../home/home';
   templateUrl: 'register.html',
 })
 export class RegisterPage {
-  email:any;
-  serial:any;
-  firstname:any;
-  lastname:any;
-  nick:any;
-  birth:any;
-
-
+  email: any;
+  serial: any;
+  firstname: any;
+  lastname: any;
+  nick: any;
+  birth: any;
   mobile: any;
   password: any;
-  re_password:any;
-  date: any;
+  re_password: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public userServices: Users, public loadingCtr: LoadingController, public alertCtrl: AlertController) {
   }
@@ -37,6 +34,14 @@ export class RegisterPage {
     let newUser = {
       mobile: this.mobile,
       password: this.password,
+      re_password: this.re_password,
+
+      email: this.email,
+      serial: this.serial,
+      firstname: this.firstname,
+      lastname: this.lastname,
+      nick: this.nick,
+      birth: this.birth
     }
 
     //loading
@@ -45,33 +50,47 @@ export class RegisterPage {
     });
     loading.present();
 
-    //DB new
-    this.userServices.userRegister(newUser).then((data) => {
+    //Password checking
+    if (newUser.password !== newUser.re_password) {
       loading.dismiss();
-      //Register fail.
-      if (typeof data["ERROR"] != "undefined") {
-        let alert = this.alertCtrl.create({
-          title: "Duplicated",
-          subTitle: "This mobile number has been registerd already.",
-          buttons: ["OK"]
-        });
-        alert.present();
-      }
-      //Register Success.
-      else {
-        let alert = this.alertCtrl.create({
-          title: "Success",
-          subTitle: "Congratulations! You are the member now.",
-          buttons: ["OK"]
-        });
-        alert.present();
-        this.navCtrl.push(HomePage, {
-          signed: true, user: data
-        });
-      }
-    }, (err) => {
-      console.log(err);
-    });
+      let alert = this.alertCtrl.create({
+        title: "Password",
+        subTitle: "Password should be same.",
+        buttons: ["OK"]
+      });
+      alert.present();
+    }
+    //Pass password checking.
+    else {
+      //DB new
+      this.userServices.userRegister(newUser).then((data) => {
+        loading.dismiss();
+        //Register fail.
+        if (typeof data["ERROR"] != "undefined") {
+          let alert = this.alertCtrl.create({
+            title: "Duplicated",
+            subTitle: "This email has been registerd already.",
+            buttons: ["OK"]
+          });
+          alert.present();
+        }
+        //Register Success.
+        else {
+          let alert = this.alertCtrl.create({
+            title: "Success",
+            subTitle: "Congratulations! You are the member now.",
+            buttons: ["OK"]
+          });
+          alert.present();
+          this.navCtrl.push(HomePage, {
+            signed: true, user: data
+          });
+        }
+      }, (err) => {
+        console.log(err);
+      });
+    }
+
   }
 
   ionViewDidLoad() {
